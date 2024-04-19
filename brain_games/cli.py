7204d8131d9b4448e60a7ae1c7e4c8584cf26fc1
AlexPatutinskii <aleks_patutinskii@mail.ru>
2024-04-19 10:21:11 +0300

    step3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+
 import prompt
+
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
@@ -9,4 +11,3 @@ def welcome_user():
 
 if __name__ == '__main__':
     welcome_user()
-
