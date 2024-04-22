d9f6f16f189f7aeb53605a6fc227d5519a0643fc
AlexPatutinskii <aleks_patutinskii@mail.ru>
2024-04-22 12:19:55 +0300

    brain_games added

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def welcome_user():
+    user_name = input('May I have your name? ')
+    print(f"Hello, {user_name}")
+
+
+if __name__ == '__main()__':
+    welcome_user()
