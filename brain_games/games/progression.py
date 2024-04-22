7c8cc84d9f6e5c5ab82dcd3ba798870b7db4f77b
AlexPatutinskii <aleks_patutinskii@mail.ru>
2024-04-23 00:21:02 +0300

    fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,7 +11,7 @@ def generate_game():
     first_n = randint(1, 30)
     delta = randint(2, 7)
     seq = list(range(first_n, first_n + delta * 10, delta))
-    taken_element_position = randint(1, 10)
+    taken_element_position = randint(0, 9)
     right_answer = seq[taken_element_position]
     question = ''
     for i in range(10):
